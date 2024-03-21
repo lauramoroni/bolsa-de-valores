9be3f67769670b45df125112ba8b110475335d0a
Maria Laura Barboza Moroni <128101943+lauramoroni@users.noreply.github.com>
2024-03-20 23:00:52 -0300

    Update color.cpp

--- color.cpp
+++ color.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 	// altera a cor do texto e do fundo do vetor indicado
 void show_color(char* text, int text_color, int backg_color) {
-	cout << "\033[1;9" << text_color << "m\033[1;4" << backg_color
+	cout << "\033[38;5;" << text_color << "m\033[48;5;" << backg_color
 		<< "m" << text << "\033[m";
 }
